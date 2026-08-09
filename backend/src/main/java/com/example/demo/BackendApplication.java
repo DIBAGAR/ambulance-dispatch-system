73dd4975df0ebb437e57da0a645541c069854820
Dibagar S <dibagar66@gmail.com>
2026-08-09 12:10:57 +0530

    fix: Add component scan to fix CORS and repository detection

--- backend/src/main/java/com/example/demo/BackendApplication.java
+++ backend/src/main/java/com/example/demo/BackendApplication.java
@@ -1,9 +1,13 @@
-package com.example.demo;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@ComponentScan(basePackages = {"com.ambulance.dispatch", "com.example.demo"})
+@EnableJpaRepositories(basePackages = "com.ambulance.dispatch.repository")
+@EntityScan(basePackages = "com.ambulance.dispatch.entity")
 public class BackendApplication {
 
 	public static void main(String[] args) {
